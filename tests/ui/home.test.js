0b9ef976ca1557b3be6836e63dbeb6c35d1aac78
borkou17 <115406400+borkou17@users.noreply.github.com>
2023-08-12 16:34:19 +0300

    Update home.test.js

--- tests/ui/home.test.js
+++ tests/ui/home.test.js
@@ -1,9 +1,9 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check home page', async ({ page }) => {
-    await page.goto('http://localhost:8080/');
+    await page.goto('https://borkou17-exam.onrender.com');
     const heading = await page.$('h1');
     const text = await heading.textContent();
-    expect(text).toContain('Welcome');  
+    expect(text).toContain('Boardgames Collection');  
   });
   
